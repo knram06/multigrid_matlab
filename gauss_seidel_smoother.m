b05d3e711b7d936de26b114083e907f5ade65d72
Ramachandran K.N <ramachandran.kn@psu.edu>
2015-12-18 13:35:37 -0500

    reproducing non-constant convergence with 2D Neumann

--- gauss_seidel_smoother.m
+++ gauss_seidel_smoother.m
@@ -18,10 +18,10 @@ invMultFactor=1/4;
 for count=1:gsIterNum
     for j=2:yLen-1
         for i=2:xLen-1
-            u(i, j, k) = invMultFactor*...
-                ( u(i-1, j, k) + u(i+1, j, k) ...
-                + u(i, j-1, k) + u(i, j+1, k) ...
-                - hSq*d(i, j, k) );
+            u(i, j) = invMultFactor*...
+                ( u(i-1, j) + u(i+1, j) ...
+                + u(i, j-1) + u(i, j+1) ...
+                - hSq*d(i, j) );
             
             %                 % update Neumann node with latest value
             %                 % so as to keep residual at zero
@@ -47,9 +47,9 @@ for count=1:gsIterNum
             
             % if on Y=0 or END faces
             if( j == 2 )
-                u(i, 1, k) = u(i, 2, k);
+                u(i, 1) = u(i, 2);
             elseif( j == yLen-1)
-                u(i, yLen, k) = u(i, yLen-1, k);
+                u(i, yLen) = u(i, yLen-1);
             end
             
         end % end of i loop
@@ -57,7 +57,7 @@ for count=1:gsIterNum
 end % end of smoother count loop
 
 % smooth edge values
-u = smooth_edge_values(u);
+%u = smooth_edge_values(u);
 
 end
 
--- smooth_edge_values.m
+++ smooth_edge_values.m
@@ -2,18 +2,14 @@ function u = smooth_edge_values(u)
 % smooth_edge_values: smoothen Neumann node locations
 %   Makes it consistent with matrix construction
 
-[xLen, yLen, zLen] = size(u);
+[xLen, yLen] = size(u);
 
 % smooth the corner points
-% 2 X lines on Y=0 Face
-for i=2:xLen-1
-    u(i, 1, 1)    = 0.5*(u(i, 2, 1) + u(i, 1, 2));
-    u(i, yLen, 1) = 0.5*(u(i, yLen-1, 1) + u(i, yLen, 2));
-    
-    u(i, 1, zLen)    = 0.5*(u(i, 2, zLen) + u(i, 1, zLen-1));
-    u(i, yLen, zLen) = 0.5*(u(i, yLen-1, zLen) + u(i, yLen, zLen-1));
-end
+u(1, 1)    = 0.5*(u(2, 1) + u(1, 2));
+u(xLen, 1) = 0.5*(u(xLen, 2) + u(xLen-1, 1));
 
+u(1, yLen)    = 0.5*(u(1, yLen-1) + u(2, yLen));
+u(xLen, yLen) = 0.5*(u(xLen-1, yLen) + u(xLen, yLen-1));
 
 end
 
