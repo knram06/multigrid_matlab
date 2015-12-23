90702956ee67651e150f0169f45928f26d417252
Ramachandran K.N <ramachandran.kn@psu.edu>
2015-12-23 13:37:45 -0500

    Carrying over R and P from previous 2D code

--- prolong_correct_error.m
+++ prolong_correct_error.m
@@ -1,25 +1,65 @@
 function ef = prolong_correct_error(ec, ef)
 
 % prolongates and corrects error
-[IF, ~] = size(ef);
+[IF, JF] = size(ef);
 
-for i = 1:IF
-    % changes from C code due to 1-indexing
-    isOnCoarseEdge = rem(i,2);
-
-    % the fine grid point is NOT on any of
-    % the coarse edges - so take the 4 neighbours
-    % and average the value
-    if(isOnCoarseEdge == 0)
-        ic = i/2;
-        retVal = 0.5*(ec(ic) + ec(ic+1) );
+for j = 1:JF
+    for i = 1:IF
+        % changes from C code due to 1-indexing
+        isOnCoarseEdge = [rem(i,2), rem(j,2)];
+        val = sum(isOnCoarseEdge);
         
-    else
-        ic = (i+1)/2;
-        retVal = ec(ic);
-    end % end of if checks for val
-    
-    ef(i) = ef(i) + retVal;
+        retVal = 0;
+        % the fine grid point is NOT on any of
+        % the coarse edges - so take the 4 neighbours
+        % and average the value
+        if(val == 0)
+            lowCoarseCorner = [i/2, j/2];
+            
+            relevantCorners = [
+                [lowCoarseCorner(1),   lowCoarseCorner(2)];
+                [lowCoarseCorner(1)+1, lowCoarseCorner(2)];
+                [lowCoarseCorner(1),   lowCoarseCorner(2)+1];
+                [lowCoarseCorner(1)+1, lowCoarseCorner(2)+1];
+                ];
+            
+            for c=1:4
+                retVal = retVal + ec(relevantCorners(c,1), relevantCorners(c,2));
+            end
+            retVal = retVal*0.25;
+            
+        elseif (val == 1)
+            coarseFaceCorners = zeros(2,2);
+            
+            % if on X Coarse face
+            if (isOnCoarseEdge(1) == 1)
+                lowCornerFace = [(i+1)/2, j/2];
+
+                coarseFaceCorners(1,:) = lowCornerFace;
+                coarseFaceCorners(2,:) = [lowCornerFace(1),   lowCornerFace(2)+1];
+                
+            % if on Y Face
+            else
+                lowCornerFace = [i/2, (j+1)/2];
+
+                coarseFaceCorners(1,:) = lowCornerFace;
+                coarseFaceCorners(2,:) = [lowCornerFace(1)+1, lowCornerFace(2)];
+            end
+            
+            for c=1:2
+                retVal = retVal + ec(coarseFaceCorners(c,1), coarseFaceCorners(c,2));
+            end
+            
+            retVal = retVal*0.5;
+
+        % coincides with coarse grid point
+        else
+            ic = (i+1)/2; jc = (j+1)/2;
+            retVal = ec(ic, jc);
+        end % end of if checks for val
+        
+        ef(i, j) = ef(i, j) + retVal;
+    end
 end
 
 end
--- restrict_residual.m
+++ restrict_residual.m
@@ -1,6 +1,12 @@
 function d = restrict_residual(r, d)
 
-[xLenC, ~] = size(d);
+nodalWeights = zeros(3,3);
+
+nodalWeights(1,:) = [0.0625, 0.125, 0.0625];
+nodalWeights(2,:) = [0.125,  0.25,  0.125];
+nodalWeights(3,:) = [0.0625, 0.125, 0.0625];
+
+[xLenC, yLenC] = size(d);
 %[xLenF, yLenF, zLenF] = size(r);
 
 % TODO: not copying over boundary faces as they should be zero
@@ -8,14 +14,28 @@ function d = restrict_residual(r, d)
 % or just use MATLAB vector syntax for this?
 %d(1, :, :) = r(1, 
 
-for ic=2:xLenC-1
-    
-    % we are effectively on the fine grid at coord
-    % (2*ic-1, 2*jc-1, 2*kc-1)
-    % local origin of cube centered at this would be
-    % (2*ic-2, 2*jc-2, 2*kc-2), basically (-1,-1,-1) of everything
-    i = 2*ic-1;
-    d(ic) = 0.25*r(i-1) + 0.5*r(i) + 0.25*r(i+1);
+for jc=2:yLenC-1
+    for ic=2:xLenC-1
+        
+        % we are effectively on the fine grid at coord
+        % (2*ic-1, 2*jc-1, 2*kc-1)
+        % local origin of cube centered at this would be
+        % (2*ic-2, 2*jc-2, 2*kc-2), basically (-1,-1,-1) of everything
+        i = 2*ic-2; j = 2*jc-2;
+        
+        % local var for calculating sum
+        val = 0;
+
+        for jStep = 0:2
+            for iStep = 0:2
+                val = val + ...
+                    (r(i+iStep, j + jStep) * ...
+                    nodalWeights(iStep+1, jStep+1) );
+            end
+        end % end of cube centered around fine grid cube with origin
+        
+        d(ic, jc) = val;
+    end
 end
 
 end
