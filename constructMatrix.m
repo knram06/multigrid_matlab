8f5a0035fd2f7e6a40c1abc4f27fc8dedfe0b5e2
Ramachandran K.N <ramachandran.kn@psu.edu>
2015-12-23 18:15:15 -0500

    Adding support for Neumann

--- constructMatrix.m
+++ constructMatrix.m
@@ -16,22 +16,22 @@ for j=1:N
     for i=1:N
         pos = nj + i;
         if(i == 1 || i == N || j == 1 || j == N)
-            mat(pos, pos) = 1;
+            %mat(pos, pos) = 1;
             
-%             if(i == 1 || i == N)
-%                 mat(pos, pos) = 1;
-%             
-%             % Neumann condition on Y planes
-%             else
-%                 mat(pos, pos) = invH;
-%                 if(j == 1)
-%                     mat(pos, pos+N) = -invH;
-%                 end
-%                 
-%                 if(j == N)
-%                     mat(pos, pos-N) = -invH;
-%                 end
-%             end
+            if(i == 1 || i == N)
+                mat(pos, pos) = 1;
+            
+            % Neumann condition on Y planes
+            else
+                mat(pos, pos) = invH;
+                if(j == 1)
+                    mat(pos, pos+N) = -invH;
+                end
+                
+                if(j == N)
+                    mat(pos, pos-N) = -invH;
+                end
+            end
         else
             mat(pos, pos-1) = oneCoeff; mat(pos, pos+1) = oneCoeff;
             mat(pos, pos-N) = oneCoeff; mat(pos, pos+N) = oneCoeff;
--- setupBoundaryConditions.m
+++ setupBoundaryConditions.m
@@ -7,38 +7,24 @@ N = sqrt(NN);
 % global extrInnerRad;
 % global extrOuterRad;
 % global capVoltage;
-% capVoltage = 0;
+capVoltage = 0;
 % global extrVoltage;
-% extrVoltage = -1350;
+extrVoltage = -1350;
 % %global center;
 
 
 arr = 0*arr;
-% enforce a BCFunc on the boudaries
+%set on X=0 face
 for j=1:N
     nj = (j-1)*N;
-    arr(nj+1) = BCFunc(0,       (j-1)*h);
-    arr(nj+N) = BCFunc((N-1)*h, (j-1)*h);
+    %ty = h*(j-1)-center(1);
+    %rr = ty*ty + tz*tz;
+    
+    %if(rr <= capRadius*capRadius)
+    arr(nj+1) = capVoltage;
+    arr(nj+N) = extrVoltage;
+    %end
 end
-
-for i=1:N
-    % j = 1
-    arr(i)           = BCFunc((i-1)*h,  0*h);
-    % j = N
-    arr((N-1)*N + i) = BCFunc((i-1)*h,  (N-1)*h);
-end
-
-% set on X=0 face
-% for j=1:N
-%     nj = (j-1)*N;
-%     %ty = h*(j-1)-center(1);
-%     %rr = ty*ty + tz*tz;
-%     
-%     %if(rr <= capRadius*capRadius)
-%     arr(nj+1) = capVoltage;
-%     arr(nj+N) = extrVoltage;
-%     %end
-% end
 
 % for i=2:I-1
 %     %ty = h*(j-1)-center(1);
@@ -49,5 +35,20 @@ end
 %     arr(i, J) = BCFunc((i-1)*h, (J-1)*h);
 %     %end
 % end
+
+% % enforce a BCFunc on the boudaries
+% for j=1:N
+%     nj = (j-1)*N;
+%     arr(nj+1) = BCFunc(0,       (j-1)*h);
+%     arr(nj+N) = BCFunc((N-1)*h, (j-1)*h);
+% end
+% 
+% for i=1:N
+%     % j = 1
+%     arr(i)           = BCFunc((i-1)*h,  0*h);
+%     % j = N
+%     arr((N-1)*N + i) = BCFunc((i-1)*h,  (N-1)*h);
+% end
+
 
 end
