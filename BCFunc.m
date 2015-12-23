35d5ac9ac7c86c7ce8e759283ca02554068574ee
Ramachandran K.N <ramachandran.kn@psu.edu>
2015-12-23 18:45:54 -0500

    commit

--- BCFunc.m
+++ BCFunc.m
@@ -1,4 +1,5 @@
-function val = BCFunc(x, y)
-val = x*x - y*y;
+function val = BCFunc(x, y, z)
+%val = x*x - y*y;
+val = x*x - 2*y*y + z*z;
 %val = 10;
 end
--- setupBoundaryConditions.m
+++ setupBoundaryConditions.m
@@ -7,24 +7,24 @@ N = sqrt(NN);
 % global extrInnerRad;
 % global extrOuterRad;
 % global capVoltage;
-capVoltage = 0;
+%capVoltage = 0;
 % global extrVoltage;
-extrVoltage = -1350;
+%extrVoltage = -1350;
 % %global center;
 
 
 arr = 0*arr;
-%set on X=0 face
-for j=1:N
-    nj = (j-1)*N;
-    %ty = h*(j-1)-center(1);
-    %rr = ty*ty + tz*tz;
-    
-    %if(rr <= capRadius*capRadius)
-    arr(nj+1) = capVoltage;
-    arr(nj+N) = extrVoltage;
-    %end
-end
+% %set on X=0 face
+% for j=1:N
+%     nj = (j-1)*N;
+%     %ty = h*(j-1)-center(1);
+%     %rr = ty*ty + tz*tz;
+%     
+%     %if(rr <= capRadius*capRadius)
+%     arr(nj+1) = capVoltage;
+%     arr(nj+N) = extrVoltage;
+%     %end
+% end
 
 % for i=2:I-1
 %     %ty = h*(j-1)-center(1);
@@ -36,19 +36,19 @@ end
 %     %end
 % end
 
-% % enforce a BCFunc on the boudaries
-% for j=1:N
-%     nj = (j-1)*N;
-%     arr(nj+1) = BCFunc(0,       (j-1)*h);
-%     arr(nj+N) = BCFunc((N-1)*h, (j-1)*h);
-% end
-% 
-% for i=1:N
-%     % j = 1
-%     arr(i)           = BCFunc((i-1)*h,  0*h);
-%     % j = N
-%     arr((N-1)*N + i) = BCFunc((i-1)*h,  (N-1)*h);
-% end
+% enforce a BCFunc on the boudaries
+for j=1:N
+    nj = (j-1)*N;
+    arr(nj+1) = BCFunc(0,       (j-1)*h);
+    arr(nj+N) = BCFunc((N-1)*h, (j-1)*h);
+end
+
+for i=1:N
+    % j = 1
+    arr(i)           = BCFunc((i-1)*h,  0*h);
+    % j = N
+    arr((N-1)*N + i) = BCFunc((i-1)*h,  (N-1)*h);
+end
 
 
 end
