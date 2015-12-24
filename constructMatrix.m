1643058f63e0b2708d1edb2c331c1eb16cedb1ee
Ramachandran K.N <ramachandran.kn@psu.edu>
2015-12-24 15:22:08 -0500

    TODO: test out Neumann changes
    FAILS at low length values! DEBUG it

--- constructMatrix.m
+++ constructMatrix.m
@@ -20,22 +20,36 @@ for k=1:N
         for i=1:N
             pos = nnk + nj + i;
             if(i == 1 || i == N || j == 1 || j == N || k == 1 || k == N)
-                mat(pos, pos) = 1;
-                
-%                 if(i == 1 || i == N)
-%                     mat(pos, pos) = 1;
-%                     
-%                     % Neumann condition on Y planes
-%                 else
-%                     mat(pos, pos) = invH;
-%                     if(j == 1)
-%                         mat(pos, pos+N) = -invH;
-%                     end
-%                     
-%                     if(j == N)
-%                         mat(pos, pos-N) = -invH;
-%                     end
-%                 end
+                if(i == 1 || i == N)
+                    mat(pos, pos) = 1;
+                    
+                % Neumann condition on Y and Z boundary planes
+                else
+                    selfCount = 0;
+                    
+                    if(j == 1)
+                        mat(pos, pos+N) = -invH;
+                        selfCount = selfCount + 1;
+                    elseif(j == N)
+                        mat(pos, pos-N) = -invH;
+                        selfCount = selfCount + 1;
+                    end
+                    
+                    if(k == 1)
+                        mat(pos, pos+NN) = -invH;
+                        selfCount = selfCount + 1;
+                    elseif(k == N)
+                        mat(pos, pos-NN) = -invH;
+                        selfCount = selfCount + 1;
+                    end
+                    
+                    % enforce the corner smoothing
+                    if(selfCount ~= 0)
+                        mat(pos, pos) = selfCount*invH;
+                    else
+                        mat(pos, pos) = invH;
+                    end
+                end
             else
                 mat(pos, pos-NN) = oneCoeff; mat(pos, pos+NN) = oneCoeff;
                 mat(pos, pos-N)  = oneCoeff; mat(pos, pos+N)  = oneCoeff;
--- setupBoundaryConditions.m
+++ setupBoundaryConditions.m
@@ -1,5 +1,8 @@
 function arr = setupBoundaryConditions(arr, h)
 
+capVoltage = 0;
+extrVoltage = -1350;
+
 [NNN, ~] = size(arr);
 
 N = uint32(NNN^(1/3));
@@ -10,26 +13,26 @@ arr = reshape(arr, [N, N, N]);
 % apply bc on X-Faces, 1 and N-1
 for k = 1:N
     for j = 1:N
-      arr(1, j, k) = BCFunc(0,       h*(j-1), h*(k-1));
-      arr(N, j, k) = BCFunc(h*(N-1), h*(j-1), h*(k-1));
+      arr(1, j, k) = capVoltage;
+      arr(N, j, k) = extrVoltage;
     end
 end
 
-% apply bc on Y-Faces
-for k = 1:N
-    for i = 1:N
-      arr(i, 1, k) = BCFunc(h*(i-1), 0,       h*(k-1));
-      arr(i, N, k) = BCFunc(h*(i-1), h*(N-1), h*(k-1));
-    end
-end
-
-% apply bc on Z-Faces
-for j = 1:N
-    for i = 1:N
-      arr(i, j, 1) = BCFunc(h*(i-1), h*(j-1), 0);
-      arr(i, j, N) = BCFunc(h*(i-1), h*(j-1), h*(N-1));
-    end
-end
+% % apply bc on Y-Faces
+% for k = 1:N
+%     for i = 1:N
+%       arr(i, 1, k) = BCFunc(h*(i-1), 0,       h*(k-1));
+%       arr(i, N, k) = BCFunc(h*(i-1), h*(N-1), h*(k-1));
+%     end
+% end
+% 
+% % apply bc on Z-Faces
+% for j = 1:N
+%     for i = 1:N
+%       arr(i, j, 1) = BCFunc(h*(i-1), h*(j-1), 0);
+%       arr(i, j, N) = BCFunc(h*(i-1), h*(j-1), h*(N-1));
+%     end
+% end
 
 % reshape before returning
 arr = reshape(arr, [N*N*N, 1]);
