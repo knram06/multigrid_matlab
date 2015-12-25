b4027f71ee681af98d1d306b55e3683fcd03b740
Ramachandran K.N <ramachandran.kn@psu.edu>
2015-12-25 16:44:59 -0500

    commit

--- test_gs.m
+++ test_gs.m
@@ -1,21 +1,38 @@
 function u = test_gs(length, numPoints)
 % Function to test Gauss-Seidel smoother
 
+global capRadius;
+capRadius = 1.326e-5;
+%capRadius = 0.044;
+global extrInnerRad;
+extrInnerRad = 1e-4;
+%extrInnerRad = 0.333;
+global extrOuterRad;
+extrOuterRad = 1.4e-4;
+%extrOuterRad = 0.4666;
+global capVoltage;
+capVoltage = 0;
+global extrVoltage;
+extrVoltage = -1350;
+global center;
+center = [length/2, length/2];
+
 h = length/(numPoints-1);
 u = zeros(numPoints, numPoints, numPoints);
 d = zeros(numPoints, numPoints, numPoints);
 
-u=setupBoundaryConditions(u, h);
-[~, norm]=calc_residual(u, d, h);
-toler=1e-6;
-cmpNorm = norm*toler;
+d=setupBoundaryConditions(d, h);
+%[~, norm]=calc_residual(u, d, h);
+rnorm = norm(reshape(d, [numPoints*numPoints*numPoints, 1]), 2);
+toler=1e-8;
+cmpNorm = rnorm*toler;
 
 iterCount=1;
 tic;
-while(norm > cmpNorm)
+while(rnorm > cmpNorm)
     u = gauss_seidel_smoother(u, d, h, 1);
-    [r, norm]=calc_residual(u, d, h);
-    fprintf('%10d %10.9g\n', iterCount, norm);
+    [r, rnorm]=calc_residual(u, d, h);
+    fprintf('%10d %10.9g\n', iterCount, rnorm);
     iterCount = iterCount+1;
 end
 toc;
